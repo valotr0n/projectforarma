1b74b8581015e3c583d60ac61666519afdb2ca54
valotr0n <beloshnez98@bk.ru>
2024-10-15 20:41:29 +0300

    Changes актуалочка

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
             setTimeout(function() {
                 window.location.href = targetUrl;
-            }, 500); // Тайм-аут для завершения анимации перед переходом
+            }, 600); // Тайм-аут для завершения анимации перед переходом
         });
     }
+});
+// Анимация при прокрутке для плавного появления секций
+document.addEventListener('DOMContentLoaded', function () {
+    const fadeElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    fadeElements.forEach(element => {
+        observer.observe(element);
+    });
 });
